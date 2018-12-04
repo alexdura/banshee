25cb0f6319d7bd8274a750662148c97de6112c15
polyvios <polyvios@ics.forth.gr>
2018-12-04 22:36:18 +0200

    port to 64bit ralloc, using 2-level trie for pages

--- banshee/libcompat/regions.c
+++ banshee/libcompat/regions.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1999-2001
  *      The Regents of the University of California.  All rights reserved.
- n*
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -43,11 +43,17 @@
 #include <stdio.h>
 #include <string.h>
 
-#define RPAGESIZE (1 << RPAGELOG)
+#define RPAGESIZE (1UL << RPAGELOG)
+#define RMAXMAP  (1UL << (RMAXMEMLOG - RMAPLOG - RPAGELOG))
 #define K 4
-#define MAXPAGE (1ULL << (RMAXMEMLOG - RPAGELOG))
+//#define MAXPAGE (1UL << (RMAXMEMLOG - RPAGELOG - RMAPLOG))
+#define RMAXPAGE  (1UL << RMAPLOG)
 
-#define PAGENB(x) (((__rcintptr)(x) >> RPAGELOG) & ((1ULL << RMAXMEMLOG) - 1))
+
+//#define PAGENB(x) (((__rcintptr)(x) >> RPAGELOG) & ((1ULL << RMAXMEMLOG) - 1))
+#define MAPNB(x) (((__rcintptr)(x) >> (RPAGELOG + RMAPLOG)) & ((1UL << (RMAXMEMLOG - RMAPLOG - RPAGELOG)) - 1))
+#define PAGENB(x) ((__rcintptr)(x) >> RPAGELOG) & ((1UL << RMAPLOG) - 1)
+
 
 #define ALIGN(x, n) (((x) + ((n) - 1)) & ~((n) - 1))
 #define PALIGN(x, n) ((void *)ALIGN((__rcintptr)(x), n))
@@ -328,7 +334,7 @@ void deleteregion_array(int n, region *regions)
 
 region regionof(void *ptr)
 {
-  return __rcregionmap[(unsigned long)ptr >> RPAGELOG];
+  return *get_or_alloc_regionmap(MAPNB(ptr), PAGENB(ptr)); //__rcregionmap[(unsigned long)ptr >> RPAGELOG];
 }
 
 void region_init(void)
@@ -377,7 +383,7 @@ static FILE *out;
 
 static void printref(void *x)
 {
-  if (x >= (void *)__rcregionmap && x < (void *)&__rcregionmap[MAXPAGE])
+  if (x >= (void *)__rcregionmap && x < (void *)&__rcregionmap[RMAXMAP])
     return;
 
 #ifdef RCPAIRS
